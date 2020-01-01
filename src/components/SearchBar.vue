7900e2f6f97b3e74bf10ebecc53e70c3adbabff6
Tanuj <tanuj.lalwani@protonmail.com>
2020-01-01 00:56:49 -0800

    [COMPONENT]: Display results in a list

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="search-bar">
-    <input />
+    <input class="search-bar__input" v-model="query" @input="search" />
+    <ul v-if="search_results" class="search-bar__results-list">
+      <li
+        class="search-bar__results-list__item"
+        v-for="result in search_results"
+        :key="result.id"
+      >{{ result.title }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import debounce from "../../node_modules/lodash/debounce";
+
 export default {
   data() {
     return {
       query: null,
       api_response: null
     };
+  },
+  computed: {
+    search_api_url() {
+      return `https://api.github.com/search/issues?q=${this.query}+repo%3Afacebook%2Freact`;
+    },
+    search_results() {
+      if (this.query) {
+        return this.api_response.items;
+      } else {
+        return [];
+      }
+    }
   },
   methods: {
     search: debounce(function() {
