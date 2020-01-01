b6bd65681729c12ae38ebf6f6a770710aa92d1fb
Tanuj <tanuj.lalwani@protonmail.com>
2020-01-01 00:56:17 -0800

    [COMPONENT]: Add Github search API call
    Using the js fetch method

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -5,7 +5,35 @@
 </template>
 
 <script>
-export default {};
+import debounce from "../../node_modules/lodash/debounce";
+export default {
+  data() {
+    return {
+      query: null,
+      api_response: null
+    };
+  },
+  methods: {
+    search: debounce(function() {
+      if (this.query) {
+        fetch(this.search_api_url, {
+          method: "GET",
+          headers: {
+            authorization: "token 029caf468d6280e5e85a83e7393c7056a423b7e9"
+          }
+        })
+          .then(response => response.json())
+          .then(data => {
+            this.api_response = data;
+          })
+          .catch(err => {
+            // eslint-disable-next-line no-console
+            console.log(err);
+          });
+      }
+    }, 100)
+  }
+};
 </script>
 
 <style>
