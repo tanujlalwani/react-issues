3b8c3f0c1b0d1e3c30c807836b680081b7ed71e0
Tanuj <tanuj.lalwani@protonmail.com>
2020-01-01 17:14:54 -0800

    [STYLE]: Add CSS reset

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,62 @@
 <template>
-  <div id="app">
+  <div class="app fill-viewport">
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+/* CSS Reset */
+@import "./styles/_reset.scss";
+
+/* Font Imports */
+@import url("https://rsms.me/inter/inter.css");
+html {
+  font-family: "Inter", sans-serif;
+}
+
+/* Font Settings */
+
+* {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+/* Layout Settings */
+
+* {
+  box-sizing: border-box;
+  -webkit-overflow-scrolling: touch;
+}
+
+/* CSS Global Style Variables */
+
+:root {
+  --font-family: "Inter", sans-serif;
+  --font-style-default: normal;
+  --font-size-default: 14px;
+  --font-size-small: 12px;
+  --font-weight-normal: 400;
+  --font-weight-bold: 600;
+}
+
+/* App Layout */
+
+html,
+body,
+.app {
+  background-color: var(--primary-color);
+  z-index: 0;
+
+  font: var(--font-style-default) var(--font-weight-normal)
+    var(--font-size-default) var(--font-family);
+}
+
+.app {
+  overflow: hidden;
+}
+
+.fill-viewport {
+  width: 100vw;
+  height: 100vh;
+}
 </style>
