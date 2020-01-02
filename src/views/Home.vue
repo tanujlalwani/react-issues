fde157e3b0097497b9fea25e145319e84667a079
Tanuj <tanuj.lalwani@protonmail.com>
2020-01-01 17:34:03 -0800

    [STYLE]: Add layout styling to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="home">
-    <!-- <img alt="React logo" src="../assets/logo.png" /> -->
+  <div class="home fill-viewport">
+    <span class="react-logo">
+      <i class="fab fa-react"></i>
+    </span>
     <search-bar></search-bar>
   </div>
 </template>
@@ -15,3 +17,41 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  .search-bar {
+    display: block;
+    max-width: 35vw;
+    margin: 10% auto 0 auto;
+
+    z-index: 100;
+  }
+
+  .react-logo {
+    position: fixed;
+    text-align: center;
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    top: 45%;
+
+    z-index: 0;
+
+    i {
+      animation: rotating 60s linear infinite;
+      font-size: 120px;
+      color: rgb(49, 224, 255);
+    }
+  }
+
+  @keyframes rotating {
+    from {
+      -webkit-transform: rotate(0deg);
+    }
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+}
+</style>
